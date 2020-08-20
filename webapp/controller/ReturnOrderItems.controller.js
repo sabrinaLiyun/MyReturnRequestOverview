f38e0f2130b4069e0a2657689403ce4be074ea8e
Jing HE <Janice.HE@cn.bosch.com>
2020-08-20 08:05:56 +0000

    Refresh adjustment

--- webapp/controller/ReturnOrderItems.controller.js
+++ webapp/controller/ReturnOrderItems.controller.js
@@ -4,46 +4,67 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator"
-], function (BaseController, MessageBox, Utilities, History,Filter, FilterOperator) {
+], function (BaseController, MessageBox, Utilities, History, Filter, FilterOperator) {
 	"use strict";
 
 	return BaseController.extend("SAPUI5MyReturnOrder.MyReturnOrderAPP.controller.ReturnOrderItems", {
-			onInit: function () {
+		onInit: function () {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getTarget("TargetReturnOrderItems").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
 
 		},
-		
+
 		handleRouteMatched: function (oEvent) {
 			var oTransfer = oEvent.getParameters().data;
 			this.fnFilterTable(oTransfer.OrderNo);
-			
+
 			var oResourceModel = new sap.ui.model.json.JSONModel();
 
 			var oParams = {};
 
-			let path = `/CustomerREturn('${oTransfer.OrderNo}')`; ///CustomerREturn('60000006')*/
+			//let path = `/CustomerREturn('${oTransfer.OrderNo}')`; ///CustomerREturn('60000006')*/
 			//var path = "/CustomerREturn('${oTransfer.OrderNo}')";
 
-			var selectedOrder = this.getView().getModel().getProperty(path);
+			// var selectedOrder = this.getView().getModel().getProperty(path);
 
 			oParams.OrderNo = oTransfer.OrderNo;
-			oParams.SalesOrg = selectedOrder.SalesOrganization;
-			oParams.CustomerNo = selectedOrder.SoldToParty;
-			oParams.CustName = selectedOrder.CustomerName;
-			oParams.DC = selectedOrder.DistributionChannel;
-			oParams.Division = selectedOrder.OrganizationDivision;
-		//	console.log("Order", selectedOrder);
-			oParams.CreationDate = selectedOrder.CreationDate;
-			oParams.ResponseDate = selectedOrder.ResponseDate;
-			oParams.Status = selectedOrder.ApprovalStatusDesc;
+			oParams.SalesOrg = oTransfer.SalesOrg;
+			oParams.CustomerNo = oTransfer.CustomerNumber;
+			oParams.CustName = oTransfer.CustomerName;
+			oParams.DC = oTransfer.Channel;
+			oParams.Division = oTransfer.Division;
+			//	console.log("Order", selectedOrder);
+
+			var lvSplitvalue1 = oTransfer.CreationDate.split("-");
+			var lvYear1 = lvSplitvalue1[0];
+			var lvMonth1 = lvSplitvalue1[1];
+			var lvDay1 = lvSplitvalue1[2];
+			var lvSAPDateFormat1 = lvYear1 + "/" + lvMonth1 + "/" + lvDay1;
+			oParams.CreationDate = lvSAPDateFormat1;
+
+			var lvSAPDateFormat2;
+
+			if (oTransfer.ResponseDate === "A") {
+				oParams.ResponseDate =" ";
+			} else {
+				var lvSplitvalue2 = oTransfer.ResponseDate.split("-");
+				var lvYear2 = lvSplitvalue2[0];
+				var lvMonth2 = lvSplitvalue2[1];
+				var lvDay2 = lvSplitvalue2[2];
+				lvSAPDateFormat2 = lvYear2 + "/" + lvMonth2 + "/" + lvDay2;
+				oParams.ResponseDate = lvSAPDateFormat2;
+
+			}
+
+			//oParams.ResponseDate =  selectedOrder.ResponseDate;
+			oParams.Status = oTransfer.Status;
 
 			oResourceModel.setDefaultBindingMode("TwoWay");
 			oResourceModel.setData(oParams);
 			this.getView().setModel(oResourceModel, "OrderResource");
 
 		},
-		
+
 		fnFilterTable: function (sFilter) {
 			//Build filter array
 			var oFilterValues = [];
@@ -56,8 +77,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(oFilterValues);
 		},
-		
-			_onFioriListReportTableUpdateFinished: function(oEvent) {
+
+		_onFioriListReportTableUpdateFinished: function (oEvent) {
 			var oTable = oEvent.getSource();
 			var oHeaderbar = oTable.getAggregation("headerToolbar");
 			if (oHeaderbar && oHeaderbar.getAggregation("content")[1]) {
@@ -70,8 +91,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 
 		},
-		
-		
+
 		formatValudationSatus: function (sValue) {
 			var statusIcon;
 			if (sValue === "Rejected") {
@@ -83,23 +103,23 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 			return statusIcon;
 		},
-		
-		formatAvailableToObjectState: function (bAvailable) { 
+
+		formatAvailableToObjectState: function (bAvailable) {
 			// return bAvailable ? "Error" : "Success";
 			var vStatus;
 			if (bAvailable === "Approved") {
 				vStatus = "Success";
 			} else if (bAvailable === "Waiting for approval") {
 				vStatus = "Warning";
-			}  else if (bAvailable === "Rejected") {
+			} else if (bAvailable === "Rejected") {
 				vStatus = "Error";
-			}  else if (bAvailable === "Partially Approved") {
+			} else if (bAvailable === "Partially Approved") {
 				vStatus = "Warning";
-			} 
-			
+			}
+
 			return vStatus;
 		},
-		
+
 		_onPageNavButtonPress: function (oEvent) {
 
 			var oBindingContext = oEvent.getSource().getBindingContext();
@@ -169,6 +189,6 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 
 		},
-	
+
 	});
 }, /* bExport= */ true);
